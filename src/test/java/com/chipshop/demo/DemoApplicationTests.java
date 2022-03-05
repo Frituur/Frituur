bb5552d517c6f695e9f739912f1dc724bdc103b5
Arno Slabbinck <arno.slabbinck@hotmail.com>
2022-03-05 13:12:12 +0100

    Initial commit

---
+++ src/test/java/com/chipshop/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chipshop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
