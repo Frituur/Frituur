2658f0ecdfd385a95881b79ee4424904b2459829
Arno Slabbinck <arno.slabbinck@hotmail.com>
2022-04-04 09:13:08 +0200

    Simple HomeController with index page

--- src/main/java/be/thomasmore/graduaten/hellospring/FrituurspringApplication.java
+++ src/main/java/be/thomasmore/graduaten/hellospring/FrituurspringApplication.java
@@ -1,12 +1,18 @@
 package be.thomasmore.graduaten.hellospring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FrituurspringApplication {
+	private static final Logger logger = LoggerFactory.getLogger(FrituurspringApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("this is a info message");
+		logger.warn("this is a warn message");
+		logger.error("this is a error message");
 		SpringApplication.run(FrituurspringApplication.class, args);
 	}
 
