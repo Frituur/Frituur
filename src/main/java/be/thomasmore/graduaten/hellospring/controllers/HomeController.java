3481b061a672e2522967716cb3d91eebeac9ecf3
RogierLa <r0807395@student.thomasmore.be>
2022-04-18 16:19:45 +0200

    gelukt eerste pagina

--- src/main/java/be/thomasmore/graduaten/hellospring/controllers/HomeController.java
+++ src/main/java/be/thomasmore/graduaten/hellospring/controllers/HomeController.java
@@ -1,15 +1,18 @@
 package be.thomasmore.graduaten.hellospring.controllers;
 
+import com.fasterxml.jackson.databind.ser.std.SqlTimeSerializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
-   public String Index() {
-        System.out.println("Hello world");
-        return "index.html";
+    @RequestMapping("/")
+    public String navigateToIndex() {
+        return "Home";
+    }
    }
-}
+
+
