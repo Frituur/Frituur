fcd7ba80b4d9aa87afddfe7d666baa0196ce91d2
Arno Slabbinck <arno.slabbinck@hotmail.com>
2022-04-28 20:31:34 +0200

    Test file

--- src/test/java/be/demo/FrituurspringApplictionTests.java
+++ src/test/java/be/demo/FrituurspringApplictionTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HellospringApplictionTests {
+class FrituurspringApplictionTests {
 
 	@Test
 	void contextLoads() {
