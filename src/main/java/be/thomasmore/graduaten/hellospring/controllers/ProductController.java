e7d5450b9d6d6e3aef7ef3d6230bed06627b0bfa
Arno Slabbinck <arno.slabbinck@hotmail.com>
2022-05-08 21:42:11 +0200

    Ordercontroller

---
+++ src/main/java/be/thomasmore/graduaten/hellospring/controllers/ProductController.java
@@ -0,0 +1,28 @@
+package be.thomasmore.graduaten.hellospring.controllers;
+
+import be.thomasmore.graduaten.hellospring.entities.Products;
+import be.thomasmore.graduaten.hellospring.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Qualifier("ProductRepository")
+    @Autowired
+    private ProductRepository repository;
+
+    public ProductController(@Qualifier("ProductRepository") ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/products")
+    List<Products> all() {
+        return repository.findAll();
+    }
+}
+
