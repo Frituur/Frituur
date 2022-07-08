6459f70f39d0b0edc1e389104904a431971d991a
RogierLa <r0807395@student.thomasmore.be>
2022-07-08 12:35:42 +0200

    Able to pass categories through controller to homepage

--- src/main/java/be/thomasmore/graduaten/hellospring/controllers/HomeController.java
+++ src/main/java/be/thomasmore/graduaten/hellospring/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package be.thomasmore.graduaten.hellospring.controllers;
 
+import be.thomasmore.graduaten.hellospring.entities.Category;
+import be.thomasmore.graduaten.hellospring.repositories.CategoryRepository;
 import be.thomasmore.graduaten.hellospring.security.UserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -8,11 +10,13 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -20,8 +24,15 @@ public class HomeController {
     @Autowired
     private UserDetailsService userDetailsService;
 
-    @GetMapping("/")
-    public String HomePage() {return "Home";}
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @RequestMapping("/")
+    public String getAllCategories(Model model) {
+        List<Category> categories=categoryRepository.findAll();
+        model.addAttribute("categories",categories);
+        return "Home";
+    }
 
     @RequestMapping("/Detail")
     public String DetailPage() {return "Detail"; }
