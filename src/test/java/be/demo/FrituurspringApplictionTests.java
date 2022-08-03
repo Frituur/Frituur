fc5b502d08a11b63d3873b8462d29ef0ef54572b
RogierLa <74931759+RogierLa@users.noreply.github.com>
2022-08-03 19:29:59 +0200

    Update FrituurspringApplictionTests.java

--- src/test/java/be/demo/FrituurspringApplictionTests.java
+++ src/test/java/be/demo/FrituurspringApplictionTests.java
@@ -8,9 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(classes = FrituurspringApplication.class)
 class FrituurspringApplictionTests {
 
-	@Test
-	void contextLoads() {
-
-	}
+	
 
 }
