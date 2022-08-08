6c661e4658c2125843514c26449dc60ad99bc322
RogierLa <r0807395@student.thomasmore.be>
2022-08-08 18:17:23 +0200

    list of orders made from selected items on homepage

--- src/main/java/be/thomasmore/graduaten/hellospring/controllers/OrderController.java
+++ src/main/java/be/thomasmore/graduaten/hellospring/controllers/OrderController.java
@@ -1,33 +1,25 @@
 package be.thomasmore.graduaten.hellospring.controllers;
 
-import be.thomasmore.graduaten.hellospring.dto.*;
+import be.thomasmore.graduaten.hellospring.dto.CategoryDto;
+import be.thomasmore.graduaten.hellospring.dto.OrderDto;
+import be.thomasmore.graduaten.hellospring.dto.ProductDto;
 import be.thomasmore.graduaten.hellospring.entities.Orders;
 import be.thomasmore.graduaten.hellospring.entities.Product;
 import be.thomasmore.graduaten.hellospring.mapper.ModelMap;
-import be.thomasmore.graduaten.hellospring.repositories.OrderRespository;
-import be.thomasmore.graduaten.hellospring.requests.RequestIds;
+import be.thomasmore.graduaten.hellospring.repositories.ProductRepository;
 import be.thomasmore.graduaten.hellospring.services.OrderService;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import static java.lang.Integer.parseInt;
 
 @Controller
 public class OrderController {
@@ -38,8 +30,11 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @GetMapping("/showorder")
-    public ModelAndView ShowOrder(@RequestParam("Order") Orders order){
+    public ModelAndView ShowOrder(@RequestParam("Order") Orders order) {
         ModelAndView mv = new ModelAndView("BestelKlant");
         OrderDto orderDto = modelMap.modelMapper().map(order, OrderDto.class);
         mv.addObject(order);
@@ -48,61 +43,70 @@ public class OrderController {
     }
 
 
-    public String FinalOrder(){
+    public String FinalOrder() {
         // Eenmaal als tijdslot gekozen is, lijst van producten in orders, naam van klant en telefoon nummer
         // Dan kan hij de final bestelling uitvoeren
 
         return "/complete";
     }
 
-    @RequestMapping(value = "/makeorder",method = RequestMethod.POST)
-    public List<Product> MakeOrder(@RequestBody ProductRequest request) throws IOException {
+    @RequestMapping(value = "/makeorder", method = RequestMethod.POST)
+    public String MakeOrder(@RequestBody String Json, RedirectAttributes ra) throws IOException {
+        String[] lijst = Json.split("r");
+        List<Orders> o = new ArrayList<>();
+        if (Json.contains("on")) {
+            for (int i = 0; i < lijst.length; i++) {
+                if (lijst[i].contains("on")) {
+                    Optional<Product> optionalEntity = productRepository.findById(Long.valueOf(i));
+                    Product product = optionalEntity.get();
+                    Orders order = new Orders();
+                    order.setProduct(product);
+                    String q = i + "=";
+                    String[] lijst2 = lijst[i].split(q);
+                    int nummer = parseInt(lijst2[1].substring(0, 1));
+                    order.setNumberOfProducts(nummer);
+                    o.add(order);
+                    return o.toString();
+                }
+            }
+        } else return "redirect:/";
 
-        List<Product> products=new ArrayList<>();
-        for(int i=0;i<request.getProducts().stream().count();i++){
-            products.add(request.getProducts().get(i));
+
+            return o.toString();
         }
-        System.out.println(products);
-        return products;
-    }
-    public static final class ProductRequest {
-        List<Product> products;
-        public List<Product> getProducts() {
-            return products;
+
+
+        //Orders ophalen van de customers in database
+        @GetMapping("/getorders")
+        public String GetOrdersFromCustomers () {
+            // get the orders from the customers
+            return "getorders";
         }
-    }
 
-    //Orders ophalen van de customers in database
-    @GetMapping("/getorders")
-    public String GetOrdersFromCustomers() {
-        // get the orders from the customers
-        return "getorders";
-    }
+        private Orders ChosenProductsForOrder (List < CategoryDto > categoryDtos, Orders order)
+        {
+            for (CategoryDto categoryDto : categoryDtos) {
+                for (ProductDto productDto : categoryDto.getProduct()) {
+                    if (productDto.isChosenProduct() == true) {
+                        Product product = modelMap.modelMapper().map(productDto, Product.class);
 
-    private Orders ChosenProductsForOrder(List<CategoryDto> categoryDtos, Orders order)
-    {
-        for (CategoryDto categoryDto : categoryDtos) {
-            for (ProductDto productDto : categoryDto.getProduct()) {
-                if(productDto.isChosenProduct() == true){
-                    Product product = modelMap.modelMapper().map(productDto, Product.class);
+                    }
 
                 }
+            }
 
+            return order;
+        }
+
+        public static final class IDRequest {
+            List<Integer> ids;
+
+            public void setIds(List<Integer> ids) {
+                this.ids = ids;
+            }
+
+            public List<Integer> getIds() {
+                return ids;
             }
         }
-
-        return order;
     }
-
-    public static final class IDRequest {
-        List<Integer> ids;
-
-        public void setIds(List<Integer> ids) {
-            this.ids = ids;
-        }
-
-        public List<Integer> getIds() {
-            return ids;
-        }
-    }
-}
