37bca13cc17c6ecb385bce945ca1d599a090d4a2
r0243928 <r0243928@student.thomasmore.be>
2022-08-23 21:31:03 +0200

    Added and adjusted AdminController & BestelAdmin page. The page now takes dynamic data from db, except orders & timeslots.

--- src/main/java/be/thomasmore/graduaten/hellospring/controllers/AdminController.java
+++ src/main/java/be/thomasmore/graduaten/hellospring/controllers/AdminController.java
@@ -3,6 +3,7 @@ package be.thomasmore.graduaten.hellospring.controllers;
 
 import be.thomasmore.graduaten.hellospring.dto.AdminDto;
 import be.thomasmore.graduaten.hellospring.dto.CategoryDto;
+import be.thomasmore.graduaten.hellospring.dto.OrderDto;
 import be.thomasmore.graduaten.hellospring.dto.ProductDto;
 import be.thomasmore.graduaten.hellospring.entities.Category;
 import be.thomasmore.graduaten.hellospring.entities.Customer;
@@ -47,22 +48,30 @@ public class AdminController {
     @Autowired
     private ProductRepository productRepository;
 
-
     @RequestMapping("/BestelAdmin")
     public String GetCategoriesAndProductsForAdminPage(Model model) {
         List<Category> categories=categoryRepository.findAll();
         List<CategoryDto> categoryDtos;
+
         List<Product> products=productRepository.findAll();
         List<ProductDto> productDtos;
-        TypeToken<List<CategoryDto>> typeTokenCategory = new TypeToken<>() {
-        };
+
+/*        List<Orders> orders=orderRepository.findAll();
+        List<OrderDto> orderDtos;
+
+        TypeToken<List<OrderDto>> typeTokenOrders = new TypeToken<>(){};
+        orderDtos = modelMap.modelMapper().map(orders, typeTokenOrders.getType());*/
+
+        TypeToken<List<CategoryDto>> typeTokenCategory = new TypeToken<>() {};
         categoryDtos = modelMap.modelMapper().map(categories,typeTokenCategory.getType());
+
         TypeToken<List<ProductDto>> typeTokenProduct = new TypeToken<>(){};
         productDtos = modelMap.modelMapper().map(products,typeTokenProduct.getType());
 
 
         model.addAttribute("categories",categoryDtos);
         model.addAttribute("products", productDtos);
+//        model.addAttribute("orders", orderDtos);
         return "BestelAdmin";
     }
 
